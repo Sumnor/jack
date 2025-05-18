273863edc7546085f519b803f0503767db33d62f
Sumnor <rodipoltavskyi@gmail.com>
2025-05-19 01:13:37 +0200

    Update Discord-Bot.py

--- Discord-Bot.py
+++ Discord-Bot.py
@@ -39,7 +39,7 @@ cached_sheet_data = []
 load_dotenv("cred.env")
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="/", intents=intents)
-bt_key = os.getenv("bot_key")
+bot_key = os.getenv("bot_key")
 API_KEY = os.getenv("API_KEY")
 YT_Key = os.getenv("YT_Key")
 commandscalled = {"_global": 0}
