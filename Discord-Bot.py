d3cce343ff7437cd1dad253be3e654074e8caa82
Sumnor <rodipoltavskyi@gmail.com>
2025-05-21 19:55:41 +0200

    Update Discord-Bot.py

--- Discord-Bot.py
+++ Discord-Bot.py
@@ -1183,7 +1183,8 @@ async def war_losses(interaction: discord.Interaction, nation_id: int, detail: s
         "nation_id": [nation_id],
         "first": 10,
         "page": 1,
-        "orderBy": [{"column": "ID", "order": "DESC"}]
+        "orderBy": [{"column": "ID", "order": "DESC"}],
+        "active": False
     }
 
     headers = {"Content-Type": "application/json"}
