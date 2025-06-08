0b46474371ed0ce318d9cdd1089047fac6374869
Sumnor <rodipoltavskyi@gmail.com>
2025-06-09 01:27:55 +0200

    Update Discord-Bot.py

--- Discord-Bot.py
+++ Discord-Bot.py
@@ -3633,13 +3633,13 @@ async def simulation(interaction: discord.Interaction, nation_id: str, war_type:
 async def who_nation(interaction: discord.Interaction, who: discord.Member):
     await interaction.response.defer()
     user_id = str(interaction.user.id)
-    user_mention = str(interaction.user.mention)
+    user_mention = str(interaction.who.id)
     async def is_banker(interaction):
         return (
             any(role.name == "Government member" for role in interaction.user.roles)
             or str(interaction.user.id) == "1148678095176474678"
         )
-    if who != user_mention:
+    if user_id != user_mention:
         if not await is_banker(interaction):
             await interaction.followup.send("❌ You don't have the rights, lil bro.")
             return
