c97a8ae1f66c95f7f81ce3bf72bc2c3cabb802e5
Sumnor <rodipoltavskyi@gmail.com>
2025-06-23 18:15:07 +0200

    Update Discord-Bot.py

--- Discord-Bot.py
+++ Discord-Bot.py
@@ -56,7 +56,7 @@ load_dotenv("cred.env")
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix="/", intents=intents)
-bot_key = os.getenv("Key")
+bot_ky = os.getenv("Key")
 API_KEY = os.getenv("API_KEY")
 YT_Key = os.getenv("YT_Key")
 commandscalled = {"_global": 0}
